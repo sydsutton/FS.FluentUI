f2e1797757915cf3de6b97d82d348c86ece6943f
Andrew <andrewsydsutton@gmail.com>
2023-09-07 09:47:04 -0500

    Rename FluentUIMigration -> V8toV9

--- src/FS.FluentUI.V8toV9/FluentUI.fs
+++ src/FS.FluentUI.V8toV9/FluentUI.fs
@@ -11,14 +11,13 @@ module FuiHelpers =
     let inline reactElement (el: ReactElementType) (props: 'a) : ReactElement = import "createElement" "react"
 
     let inline createElement (el: ReactElementType) (props: 'ControlProperty list) : ReactElement = reactElement el (!!props |> createObj)
-    let [<Literal>] FluentUIMigration = "@fluentui/react-migration-v8-v9"
+    let [<Literal>] V8toV9 = "@fluentui/react-migration-v8-v9"
 
 type [<Erase>] Fui =
 
     /// Creates v9 brand colors from a v8 palette using interpolation.
     /// A v8 palette has nine colors and v9 has sixteen colors.
-    static member inline createBrandVariants (theme: IPalette, interpolation: Interpolation option): 'T = import "createBrandVariants" FluentUIMigration
+    static member inline createBrandVariants (theme: IPalette, interpolation: Interpolation option): 'T = import "createBrandVariants" V8toV9
 
-    // Stack
-    static member inline stack (props: IStackProp list) = createElement (import "StackShim" FluentUIMigration) props
-    static member inline stackItem (props: IStackItemProp list) = createElement (import "StackItemShim" FluentUIMigration) props
+    static member inline stack (props: IStackProp list) = createElement (import "StackShim" V8toV9) props
+    static member inline stackItem (props: IStackItemProp list) = createElement (import "StackItemShim" V8toV9) props
