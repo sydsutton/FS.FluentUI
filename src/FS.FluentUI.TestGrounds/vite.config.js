0e45c9a2695dd87f4119034c8a43df24eb38566f
Andrew <andrewsydsutton@gmail.com>
2023-09-07 11:55:55 -0500

    Replace Webpack with Vite

---
+++ src/FS.FluentUI.TestGrounds/vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+/** @type {import('vite').UserConfig} */
+export default defineConfig({
+    plugins: [react()],
+    root: "./src",
+    build: {
+        outDir: "../dist",
+    }
+})
