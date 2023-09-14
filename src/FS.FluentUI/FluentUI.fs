6018574d237c56f224415ae8d73cc100c9a911fe
Andrew <andrewsydsutton@gmail.com>
2023-09-14 15:52:20 -0500

    Add warning for Fui.table in description

--- src/FS.FluentUI/FluentUI.fs
+++ src/FS.FluentUI/FluentUI.fs
@@ -491,12 +491,12 @@ type [<Erase>] Fui =
     static member inline menuItemLink (props: IMenuItemLinkProp list) = createElement (import "MenuItemLink" FluentUIv9) props
     // Table
     /// This component is considered low-level and should be used when there is a need for more customization and support for non-standard features. Please check out the DataGrid component if you don't need lots of customization and rely on common features. There is less work involved and you will benefit from first class Microsoft design and accessibility support.
-    /// A Table displays sets of two-dimensional data. The primitive components can be fully customized to support different feature sets. The library provides a default useTableFeatures hook that handles the business logic and state management of common features. There is no obligation to use our hook with these components, we've created it for convenience.
-    /// The useTableFeatures hook was designed with feature composition in mind. This means that they are composed using plugins hooks such as useTableSort and useTableSelection as a part of useTableFeatures. This means that as the feature set expands, users will not need to pay the bundle size price for features that they do not intend to use. Please consult the usage examples below for more details.
+    ///
+    /// WARNING: Hooks for this component are incomplete. Please use ```Fui.dataGrid``` instead.
     static member inline table (props: ReactElement list) = Interop.reactElementWithChildren (import "Table" FluentUIv9) props
     /// This component is considered low-level and should be used when there is a need for more customization and support for non-standard features. Please check out the DataGrid component if you don't need lots of customization and rely on common features. There is less work involved and you will benefit from first class Microsoft design and accessibility support.
-    /// A Table displays sets of two-dimensional data. The primitive components can be fully customized to support different feature sets. The library provides a default useTableFeatures hook that handles the business logic and state management of common features. There is no obligation to use our hook with these components, we've created it for convenience.
-    /// The useTableFeatures hook was designed with feature composition in mind. This means that they are composed using plugins hooks such as useTableSort and useTableSelection as a part of useTableFeatures. This means that as the feature set expands, users will not need to pay the bundle size price for features that they do not intend to use. Please consult the usage examples below for more details.
+    ///
+    /// WARNING: Hooks for this component are incomplete. Please use ```Fui.dataGrid``` instead.
     static member inline table (props: ITableProp list) = createElement (import "Table" FluentUIv9) props
     static member inline tableHeader (props: ReactElement list) = Interop.reactElementWithChildren (import "TableHeader" FluentUIv9) props
     static member inline tableHeader (props: ITableHeaderProp list) = createElement (import "TableHeader" FluentUIv9) props
