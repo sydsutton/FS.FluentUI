6b575805af36f173d7717d9b0994d2dbc8c12a21
Finn Olav Sagen <fo-sagen@drsagen.no>
2026-04-05 12:00:02 +0200

    FS.FluentUI.V8toV9 to Felizv3

--- src/FS.FluentUI.V8toV9/Utils.fs
+++ src/FS.FluentUI.V8toV9/Utils.fs
@@ -1,6 +1,8 @@
 namespace FS.FluentUI.V8toV9
 
 open Fable.Core
+open Fable.Core.JsInterop
+open Fable.React
 
 type [<Erase>] IStackProp = interface end
 type [<Erase>] IStackItemProp = interface end
@@ -11,3 +13,5 @@ type [<Erase>] IStackTokens = interface end
 [<RequireQualifiedAccess>]
 type Interop =
     static member inline mkProperty<'ControlProperty> (key:string) (value:obj) : 'ControlProperty = unbox (key, value)
+    static member inline reactElementWithChildren (el: ReactElementType) (children: #seq<ReactElement>) : ReactElement =
+        ReactBindings.React.createElement(el, createObj [], children)
